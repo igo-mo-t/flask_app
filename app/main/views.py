ef33a2c6a346373680991a6c275ca3ffe41ed85b
IgorR <igorrozhnov95@gmail.com>
2022-11-01 20:30:26 +0300

    b1

--- app/main/views.py
+++ app/main/views.py
@@ -79,7 +79,7 @@ def article():
 @main.route('/visits-counter/')
 def visits():
     if 'visits' in session:
-        # чтение и обновление данных сессии yeee
+        # чтение и обновление данных сессии BUGGG
         session['visits'] = session.get('visits') + 1
     else:
         session['visits'] = 1  # настройка данных сессии
